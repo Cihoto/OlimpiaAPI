a415824d4bd74a864570a453d3705508ee1c81e8
Cihoto <coteloyola@hotmail.com>
2025-04-17 09:25:07 -0400

    new endpoint webhook

--- src/routes/botRoutes.js
+++ src/routes/botRoutes.js
@@ -3,17 +3,19 @@ const router = Router();
 
 router.post('/sendMailToWebHook', async (req, res) => {
     const {email} = req.body;
-    const response = await fetch ('https://services.leadconnectorhq.com/hooks/vdenYRGQMAGUqXLAvk8N/webhook-trigger/573f2607-af9c-4e2d-99c0-4801a12f1fb8 ', {
+    const response = await fetch ('https://services.leadconnectorhq.com/hooks/vdenYRGQMAGUqXLAvk8N/webhook-trigger/ce76f359-1e6c-4fd1-894d-e5f57d67b533', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(email)
-    })
+    });
     const data = await response.json();
     console.log("data", data);
     res.status(200).json(data);
     return
-})
+});
+
+
 
 export default router;
