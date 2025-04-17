9085124059c7642dfc5954e245b2faddfd674d18
Cihoto <coteloyola@hotmail.com>
2025-04-17 09:45:31 -0400

    update logs and response on bot

--- src/routes/botRoutes.js
+++ src/routes/botRoutes.js
@@ -3,6 +3,7 @@ const router = Router();
 
 router.post('/sendMailToWebHook', async (req, res) => {
     const {email} = req.body;
+    console.log("email",email)
     const response = await fetch ('https://services.leadconnectorhq.com/hooks/vdenYRGQMAGUqXLAvk8N/webhook-trigger/ce76f359-1e6c-4fd1-894d-e5f57d67b533', {
         method: 'POST',
         headers: {
@@ -12,7 +13,7 @@ router.post('/sendMailToWebHook', async (req, res) => {
     });
     const data = await response.json();
     console.log("data", data);
-    res.status(200).json(data);
+    res.status(200).json({data,email});
     return
 });
 
