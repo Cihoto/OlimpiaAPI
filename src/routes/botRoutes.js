46521c601d18b10d3c9f3bb4d66219a918b51086
Cihoto <coteloyola@hotmail.com>
2025-04-17 10:28:35 -0400

    new foundSystem

--- src/routes/botRoutes.js
+++ src/routes/botRoutes.js
@@ -1,4 +1,12 @@
 import { Router } from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const jsonFilePath = path.join(__dirname, "../documents/jsonBOT.json");
+
 const router = Router();
 
 router.post('/sendMailToWebHook', async (req, res) => {
@@ -11,6 +19,12 @@ router.post('/sendMailToWebHook', async (req, res) => {
         },
         body: JSON.stringify(req.body)
     });
+
+    writeToJsonFile({
+        email: req.body.email,
+        url: ""
+    });
+
     const data = await response.json();
     console.log("data", data);
     res.status(200).json({data,email});
@@ -18,5 +32,94 @@ router.post('/sendMailToWebHook', async (req, res) => {
 });
 
 
+router.post('/updateurl', async (req, res) => {
+    const {email, url} = req.body;
+
+    const updated = updateClientByEmail(email,url)
+    res.status(200).json(updated);
+    return
+});
+
+router.post('/getData', async (req, res) => {
+    const {email} = req.body;
+    const data = getDataWithMail(email)
+    res.json(data);
+    return
+});
+
+router.post('/deleteData', async (req, res) => {
+    const {email} = req.body;
+    const data = removeDataFromJson(email)
+    res.json(data);
+    return
+})
+
+const writeToJsonFile = (data) => {
+    try {
+        const jsonData = fs.existsSync(jsonFilePath) ? fs.readFileSync(jsonFilePath, "utf8") : "[]";
+        const parsedData = JSON.parse(jsonData);
+        parsedData.push(data);
+        fs.writeFileSync(jsonFilePath, JSON.stringify(parsedData, null, 2), "utf8");
+        console.log("Archivo JSON actualizado correctamente.");
+    } catch (error) {
+        console.error("Error al escribir en el archivo JSON:", error);
+    }
+};
+
+const updateClientByEmail = (email,url) => {
+    try {
+        const jsonData = fs.readFileSync(jsonFilePath, "utf8");
+        const parsedData = JSON.parse(jsonData);
+        console.log("parsedData", parsedData)
+        const result = parsedData.find(item => item.email == email);
+        // return result
+        if (result) {
+            console.log("Email encontrado:", result);
+
+            result.url = url;
+            fs.writeFileSync(jsonFilePath, JSON.stringify(parsedData, null, 2), "utf8");
+            return result;
+        } else {
+            console.log("Email no encontrado.");
+            return null;
+        }
+    } catch (error) {
+        console.error("Error al leer el archivo JSON:", error);
+        return null;
+    }
+};
+
+function getDataWithMail(email) {
+    try {
+        const jsonData = fs.readFileSync(jsonFilePath, "utf8");
+        const parsedData = JSON.parse(jsonData);
+        // return parsedData
+        const result = parsedData.find(item => item.email == email);
+        if (result) {
+            // console.log("Email encontrado:", result);
+            return result;
+        } else {
+            // console.log("Email no encontrado.");
+            return null;
+        }
+    } catch (error) {
+        console.error("Error al leer el archivo JSON:", error);
+        return null;
+    }
+}
+
+function removeDataFromJson(email) {
+    try {
+        const jsonData = fs.readFileSync(jsonFilePath, "utf8");
+        const parsedData = JSON.parse(jsonData);
+        const filteredData = parsedData.filter(item => item.email !== email);
+        fs.writeFileSync(jsonFilePath, JSON.stringify(filteredData, null, 2), "utf8");
+        return true
+        console.log("Email eliminado correctamente.");
+    } catch (error) {
+        return false
+        console.error("Error al leer el archivo JSON:", error);
+    }
+}
 
 export default router;
